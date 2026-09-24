33d01821cb7452417417c2fa99e4f396e35155af
tanzer <tanzercakir@gmail.com>
2026-09-24 14:22:44 +0300

    Implement ReportBuilder Run 3 initial solution

---
+++ research/us_language/trials/us/run_3/initial/solution/report_builder/__init__.py
@@ -0,0 +1,16 @@
+"""Public package for the ReportBuilder trial implementation."""
+
+from .data import DataReader
+from .render import Renderer
+from .results import EmptyReport, ReadFailure
+from .service import ReportService
+from .ui import UI
+
+__all__ = [
+    "DataReader",
+    "Renderer",
+    "ReadFailure",
+    "EmptyReport",
+    "ReportService",
+    "UI",
+]
