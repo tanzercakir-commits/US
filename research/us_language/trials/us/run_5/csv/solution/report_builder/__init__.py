68df7c8f19586b01165025fa4ccc4b5692af1fd3
tanzer <tanzercakir@gmail.com>
2026-09-24 20:49:29 +0300

    Implement Run 5 CSV report output

---
+++ research/us_language/trials/us/run_5/csv/solution/report_builder/__init__.py
@@ -0,0 +1,14 @@
+from .data import DataReader
+from .render import Renderer
+from .results import EmptyReport, ReadFailure
+from .service import ReportService
+from .ui import UI
+
+__all__ = [
+    "DataReader",
+    "Renderer",
+    "EmptyReport",
+    "ReadFailure",
+    "ReportService",
+    "UI",
+]
